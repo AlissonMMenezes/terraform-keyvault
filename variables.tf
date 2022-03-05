735cfef611b8e8d669ddc6d9b65bed269f23eb12
Alisson Menezes <Alisson.itops@gmail.com>
2022-03-05 16:47:10 +0100

    creating kv module

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "key_vault_name" {
+  type        = string
+  description = "Storage Account Name"
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "Resource Group Name"
+}
+
+variable "location" {
+  type        = string
+  description = "Resource Group Location"
+  default     = "West Europe"
+}
